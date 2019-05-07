c17b3b41551dbe01addf131040d6c62a71a2bc94
jack morris <jack.morris@kainos.com>
2019-05-07 14:39:00 +0100

    add setup.sh and update README/folder structure

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# Installing Brew Packages
-Install all of the packages mentioned in the `brew.txt` file by running `xargs brew install < brew.txt`
+# Setup My Fking Mac
+Put a little seasoning on your shiny new Mac so you can get to work faster. Run the provided `setup` script to install the following:
+- Homebrew
+- iTerm2
+- oh-my-zsh
+- powerlevel9k (custom theme for oh-my-zsh)
+- and whichever packages you specify in the provided .txt file!
 
-# Sample Directory structure
-Included is a sample directory structure that can be copied directly into your $HOME folder to get yourself going. The `README`'s that are included are just so git preserves the folder structure. You can remove them once you have copied them into your $HOME folder by running `find . -name 'README.md' -delete`
+This also creates a sample folder structure which you have the option of copying over into your home folder.
+
+```
+.
+├── aws
+├── azure
+├── go_workspaces
+│   └── main
+│       ├── bin
+│       ├── pkg
+│       └── src
+└── python
+```
+The READMEs that are included are just so git preserves the folder structure. You can remove them by running `find . -name 'README.md' -delete`.
+
+# Automated Install
+This is recommended only if you haven't setup your machine at all. 
+
+*Note:* While this won't hurt or overwrite existing packages installed by brew, it may remove plugins or themes you have already installed in the `~/.oh-my-zsh/custom/themes` directory.
+
+# Manual Steps (if you so choose)
+## Installing Brew Packages
+Install all of the packages mentioned in the `brew.txt` file by running `xargs brew install < brew.txt`
---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Install command line tools
+if xcode-select -v > /dev/null; then
+  printf '\e[0;33m%-6s\e[m' "[XCODE] command line tools already installed."
+else
+  printf '\e[0;33m%-6s\e[m' "[XCODE] installing command line tools."
+  xcode-select --install
+fi
+
+# Install brew
+if brew -v > /dev/null; then
+  printf '\e[0;33m%-6s\e[m' "[HOMEBREW] homebrew is already installed."
+else
+  printf '\e[0;33m%-6s\e[m' "[HOMEBREW] homebrew is not installed, proceeding with install."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null || exit 1
+  printf '\e[0;32m%-6s\e[m' "[HOMEBREW] homebrew successfully installed."
+fi
+
+# Install packages listed in brew.txt, including zsh
+printf '\e[0;33m%-6s\e[m' "[HOMEBREW] installing packages..."
+args brew install < brew.txt
+
+# Install iTerm2
+printf '\e0;33m%-6s\e[m' "[ITERM2] installing iTerm2..."
+brew cask install iterm2
+
+# Install oh-my-zsh -- using a fork of the original oh-my-zsh to avoid launching zsh after installer finishes
+printf '\e[0;33m%-6s\e[m' "[ZSH] installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Liquidsoul/oh-my-zsh/master/tools/install.sh)"
+if $? -eq 0; then
+  printf '\e[0;32m%-6s\e[m' "[ZSH] oh-my-zsh successfully installed."
+else
+  printf '\e[0;31m%-6s\e[m' "[ZSH] oh-my-zsh failed to install."
+  exit 1
+fi
+
+# Install powerlevel9k theme + custom fonts
+printf '\e[0;33m%-6s\e[m' "[POWERLEVEL9K] downloading powerlevel9k..."
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+printf '\e[0;33m%-6s\e[m' "[POWERLEVEL9K] downloading powerline fonts..."
+git clone https://github.com/powerline/fonts.git ~/powerlinefonts
+printf '\e[0;33m%-6s\e[m' "[POWERLEVEL9K] installing powerline fonts..."
+sh ~/powerlinefonts/install.sh
+
+# Overwrite default ~/.zshrc with the one contained in this repo
+mv .zshrc ~/.zshrc
