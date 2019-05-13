a79a14ef33b8dd0a9c0921d5b700647b50d3a91e
jack morris <jack.morris@kainos.com>
2019-05-13 14:28:43 +0100

    fix printf statements and remove error checking for zsh install

--- setup.sh
+++ setup.sh
@@ -1,59 +1,47 @@
 #!/bin/bash
 # Install command line tools
 if xcode-select -v > /dev/null; then
-  printf '\e[0;33m%-6s\e[m' "[XCODE] command line tools already installed."
+  printf '\e[0;33m%-6s\e[m\n' "[XCODE] command line tools already installed."
 else
-  printf '\e[0;33m%-6s\e[m' "[XCODE] installing command line tools."
+  printf '\e[0;33m%-6s\e[m\n' "[XCODE] installing command line tools."
   xcode-select --install
 fi
 
 # Install brew
 if brew -v > /dev/null; then
-  printf '\e[0;33m%-6s\e[m' "[HOMEBREW] homebrew is already installed."
+  printf '\e[0;33m%-6s\e[m\n' "[HOMEBREW] homebrew is already installed."
 else
-  printf '\e[0;33m%-6s\e[m' "[HOMEBREW] homebrew is not installed, proceeding with install."
+  printf '\e[0;33m%-6s\e[m\n' "[HOMEBREW] homebrew is not installed, proceeding with install."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null || exit 1
-  printf '\e[0;32m%-6s\e[m' "[HOMEBREW] homebrew successfully installed."
+  printf '\e[0;32m%-6s\e[m\n' "[HOMEBREW] homebrew successfully installed."
 fi
 
 # Install packages listed in brew.txt, including zsh
-printf '\e[0;33m%-6s\e[m' "[HOMEBREW] installing packages..."
+printf '\e[0;33m%-6s\e[m\n' "[HOMEBREW] installing packages..."
 xargs brew install < brew.txt
 
 # Install iTerm2
-printf '\e0;33m%-6s\e[m' "[ITERM2] installing iTerm2..."
+printf '\e0;33m%-6s\e[m\n' "[ITERM2] installing iTerm2..."
 brew cask install iterm2
 
 # Install oh-my-zsh -- using a fork of the original oh-my-zsh to avoid launching zsh after installer finishes
-printf '\e[0;33m%-6s\e[m' "[ZSH] installing oh-my-zsh..."
+printf '\e[0;33m%-6s\e[m\n' "[ZSH] installing oh-my-zsh..."
 if [[ $TRAVIS == true ]]; then
   curl -fsSL https://raw.githubusercontent.com/rudolphjacksonm/oh-my-zsh/master/tools/install.sh > install.sh
   chmod +x install.sh
   ./install.sh --silent --skip-chsh
-  if $? -eq 0; then
-    printf '\e[0;32m%-6s\e[m' "[ZSH] oh-my-zsh successfully installed."
-  else
-    printf '\e[0;31m%-6s\e[m' "[ZSH] oh-my-zsh failed to install."
-    exit 1
-  fi
 else
   curl -fsSL https://raw.githubusercontent.com/rudolphjacksonm/oh-my-zsh/master/tools/install.sh > install.sh
   chmod +x install.sh
   ./install.sh --silent
-  if $? -eq 0; then
-    printf '\e[0;32m%-6s\e[m' "[ZSH] oh-my-zsh successfully installed."
-  else
-    printf '\e[0;31m%-6s\e[m' "[ZSH] oh-my-zsh failed to install."
-    exit 1
-  fi
 fi
 
 # Install powerlevel9k theme + custom fonts
-printf '\e[0;33m%-6s\e[m' "[POWERLEVEL9K] downloading powerlevel9k..."
+printf '\e[0;33m%-6s\e[m\n' "[POWERLEVEL9K] downloading powerlevel9k..."
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-printf '\e[0;33m%-6s\e[m' "[POWERLEVEL9K] downloading powerline fonts..."
+printf '\e[0;33m%-6s\e[m\n' "[POWERLEVEL9K] downloading powerline fonts..."
 git clone https://github.com/powerline/fonts.git ~/powerlinefonts
-printf '\e[0;33m%-6s\e[m' "[POWERLEVEL9K] installing powerline fonts..."
+printf '\e[0;33m%-6s\e[m\n' "[POWERLEVEL9K] installing powerline fonts..."
 sh ~/powerlinefonts/install.sh
 
 # Overwrite default ~/.zshrc with the one contained in this repo
